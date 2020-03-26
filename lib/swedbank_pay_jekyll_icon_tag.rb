0f97e815565a2ee9849864ff76c36f9bb575ae09
Nullint <daniel.granerud@payex.com>
2020-03-26 10:23:43 +0100

    It works!

--- lib/swedbank_pay_jekyll_icon_tag.rb
+++ lib/swedbank_pay_jekyll_icon_tag.rb
@@ -1,16 +1,14 @@
 require "swedbank_pay_jekyll_icon_tag/version"
 require 'Liquid'
 
-module SwedbankPayjekyllIconTag
-  class IconTag < Liquid::Tag
-    def initialize(tag_name, text, tokens)
-      super
-      @text = text
-    end
+class IconTag < Liquid::Tag
+  def initialize(tag_name, text, tokens)
+    super
+    @text = text
+  end
 
-    def render(_)
-      "<i class=\"material-icons\">#{@text.strip}</i>"
-    end
+  def render(_)
+    "<i class=\"material-icons\">#{@text.strip}</i>"
   end
   
   Liquid::Template.register_tag "icon", self
